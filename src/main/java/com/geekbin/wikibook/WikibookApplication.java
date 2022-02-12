13040d3e0632a743d08270362793e641a1b5da91
geeksuperbin <liubindashen@gmail.com>
2022-02-13 06:12:32 +0800

    修改 SpringBoot 启动 Application 显示信息

--- src/main/java/com/geekbin/wikibook/WikibookApplication.java
+++ src/main/java/com/geekbin/wikibook/WikibookApplication.java
@@ -1,13 +1,22 @@
 package com.geekbin.wikibook;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikibookApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikibookApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikibookApplication.class, args);
+        SpringApplication app = new SpringApplication(WikibookApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
+
 
 }
