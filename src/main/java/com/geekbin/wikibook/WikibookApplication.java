ba6f386aaf27bff393a7d7c8699158390d4a9e11
刘彬 <hhdxlb@qq.com>
2022-02-13 04:42:06 +0800

    2-2 init 初始化 SprintBoot 项目

---
+++ src/main/java/com/geekbin/wikibook/WikibookApplication.java
@@ -0,0 +1,13 @@
+package com.geekbin.wikibook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikibookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikibookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geekbin/wikibook/WikibookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbin.wikibook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikibookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
