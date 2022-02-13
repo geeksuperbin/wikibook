dc64bec22c969918506c1bd38c18e2e8ced075b6
geeksuperbin <liubindashen@gmail.com>
2022-02-13 10:46:49 +0800

    增加 hello 接口的 IDE 测试文件 test.http 并设置 http 断言

--- src/main/java/com/geekbin/wikibook/controller/TestController.java
+++ src/main/java/com/geekbin/wikibook/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.geekbin.wikibook.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,13 @@ public class TestController {
     public  String hello(){
         return "Hello World";
     }
+
+
+    @PostMapping("/hello/post")
+    public  String helloPost(String name) {
+
+        return "Hello Worlo ! Post, " + name;
+    }
+
+
 }
