2f423948dd006d0d571889429aebe2d5eff8e9fa
geeksuperbin <liubindashen@gmail.com>
2022-02-13 21:37:34 +0800

    增加 自定义配置项和对 @Value 无配置值时设置默认值

--- src/main/java/com/geekbin/wikibook/controller/TestController.java
+++ src/main/java/com/geekbin/wikibook/controller/TestController.java
@@ -1,11 +1,16 @@
 package com.geekbin.wikibook.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private  String testHello;
+
 
     /*
         /user?id=1
@@ -21,7 +26,7 @@ public class TestController {
     @PostMapping("/hello/post")
     public  String helloPost(String name) {
 
-        return "Hello Worlo ! Post, " + name;
+        return "Hello Worlo ! Post, " + name + testHello;
     }
 
 
