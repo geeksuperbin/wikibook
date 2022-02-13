83447638a0a89d098761b756c15d5fc2a83e9b67
geeksuperbin <liubindashen@gmail.com>
2022-02-13 10:22:11 +0800

    增加 url传参get方式和rest get 方式的区别

--- src/main/java/com/geekbin/wikibook/controller/TestController.java
+++ src/main/java/com/geekbin/wikibook/controller/TestController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    /*
+        /user?id=1
+        等价于
+        /user/1
+     */
     @GetMapping("/hello")
     public  String hello(){
         return "Hello World";
